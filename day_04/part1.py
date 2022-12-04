cb8b99eec2a2bd853a8a6a0d73fac478dff6c35d
Charles EM Reynolds <reynoldscem@gmail.com>
2022-12-04 09:25:12 +0000

    Day 4 part 1

---
+++ day_04/part1.py
@@ -0,0 +1,45 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import re
+
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-i', '--input-filename', type=Path,
+        required=True
+    )
+
+    return parser
+
+
+def set_from_exclusive_endpoints(start, end):
+    return set(range(start, end + 1))
+
+
+def line_to_set_pair(line):
+    match_object = re.match(r'(\d+)-(\d+),(\d+)-(\d+)', line)
+    match_ints = map(int, match_object.groups())
+    first_start, first_end, second_start, second_end = match_ints
+
+    return (
+        set_from_exclusive_endpoints(first_start, first_end),
+        set_from_exclusive_endpoints(second_start, second_end)
+    )
+
+
+def one_contains_other(first, second):
+    return first.issubset(second) or second.issubset(first)
+
+
+def main():
+    args = build_parser().parse_args()
+
+    with open(args.input_filename) as fd:
+        lines = fd.read().splitlines()
+
+    print(sum(one_contains_other(*line_to_set_pair(line)) for line in lines))
+
+
+if __name__ == '__main__':
+    main()
