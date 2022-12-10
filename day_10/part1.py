ac753fc5eb6d13c59ed0442ec770ad47ec80691f
Charles EM Reynolds <reynoldscem@gmail.com>
2022-12-10 12:08:37 +0000

    Day 10 part 1 stub

---
+++ day_10/part1.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-i', '--input-filename', type=Path,
+        required=True
+    )
+
+    return parser
+
+
+def main():
+    args = build_parser().parse_args()
+
+    with open(args.input_filename) as fd:
+        data = fd.read()
+
+    lines = data.splitlines()
+    lines
+
+
+if __name__ == '__main__':
+    main()
