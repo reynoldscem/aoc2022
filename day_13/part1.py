d5263974c3d41bb517f9da772ab8061a80357711
Charles EM Reynolds <reynoldscem@gmail.com>
2022-12-13 10:42:30 +0000

    Checkpointing Day 13 part 1 for posterity.

---
+++ day_13/part1.py
@@ -0,0 +1,112 @@
+from argparse import ArgumentParser
+from itertools import count
+from pathlib import Path
+import json
+
+from enum import Enum
+
+
+class Comparison(Enum):
+    LESSER = 1
+    EQUAL = 2
+    GREATER = 3
+
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-i', '--input-filename', type=Path,
+        required=True
+    )
+
+    return parser
+
+
+def parse_string_to_pair(string):
+    return tuple(
+        json.loads(string_part)
+        for string_part in string.split('\n')
+    )
+
+
+def compare(left, right, level=0):
+
+    print('  ' * level, end='')
+    print(f'- Compare {left} vs {right}')
+    if left == right:
+        return Comparison.EQUAL
+    elif left < right:
+        print('  ' * level, end='')
+        print('- Left side is smaller, so input are in the right order')
+        return Comparison.LESSER
+    elif left > right:
+        print('  ' * level, end='')
+        print(
+            '- Right side is smaller, '
+            'so inputs are not in the right order'
+        )
+        return Comparison.GREATER
+
+
+def ordered(left, right, level=0):
+    print(f'- Compare {left} vs {right}')
+    for a, b in zip(left, right):
+        comparison = compare(a, b, level=level + 1)
+        if comparison == Comparison.EQUAL:
+            pass
+        elif comparison == Comparison.LESSER:
+            return True
+        elif comparison == Comparison.GREATER:
+            return False
+        else:
+            raise RuntimeError("Invalid comparison")
+
+    return len(left) > len(right)
+
+    # if isinstance(left, int) and isinstance(right, int):
+    #     return left < right
+
+    # if isinstance(left, list) and isinstance(right, list):
+    #     is_ordered = True
+    #     while True:
+    #         if len(left) == 0:
+    #             return is_ordered
+    #         elif len(right) == 0:
+    #             return False
+
+    #         left_element = left.pop(0)
+    #         right_element = right.pop(0)
+
+
+def main():
+    args = build_parser().parse_args()
+
+    with open(args.input_filename) as fd:
+        data = fd.read().strip()
+
+    pair_strings = data.split('\n\n')
+    pairs = [parse_string_to_pair(string) for string in pair_strings]
+
+    pairs_with_indices = list(zip(count(1), pairs))
+
+    # indices_of_pairs_in_order = [
+    #     index
+    #     for index, (first, second) in pairs_with_indices
+    #     if ordered(first, second)
+    # ]
+
+    # print(sum(indices_of_pairs_in_order))
+
+    pairs_with_indices
+    for index, (first, second) in pairs_with_indices:
+        print(f'== Pair {index} ==')
+        try:
+            ordered(first, second)
+        except Exception as e:
+            print('This case failed')
+            print(e)
+            print()
+
+
+if __name__ == '__main__':
+    main()
